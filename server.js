465579cd2662d2e3b9b6f89c171fe4914c7302c5
catbyte-io <catbyte.io@gmail.com>
2025-01-01 21:05:01 -0700

    add bootstrap

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 6060;
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('index.ejs');
 });
 
 app.get('/the_prophecies_begin', (req, res) => {
     res.send('The Prophecies Begin')
-})
+});
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
