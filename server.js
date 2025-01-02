c4783fa4ec73355aa508df3011cf0ed6fa913a46
catbyte-io <catbyte.io@gmail.com>
2025-01-01 20:48:24 -0700

    Install axios

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 6060;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.get('/the_prophecies_begin', (req, res) => {
+    res.send('The Prophecies Begin')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
