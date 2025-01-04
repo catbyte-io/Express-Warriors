ef8f8e1c1ad6661b8e41f498543f062126014c28
catbyte-io <catbyte.io@gmail.com>
2025-01-04 15:41:39 -0700

    import style sheets and bootstrap

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+// Define application
 const express = require('express');
 const app = express();
-const port = 6060;
 
+// Use bootstrap and custom css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './public/styles.css';
+
+// Configure app to use nunjucks for templating
 const nunjucks = require('nunjucks');
 
 nunjucks.configure('views', {
@@ -9,6 +15,7 @@ nunjucks.configure('views', {
   express: app
 });
 
+// Routes
 app.get('/', (req, res) => {
   res.render('base.html');
 });
@@ -17,8 +24,11 @@ app.get('/the_prophecies_begin', (req, res) => {
     res.send('The Prophecies Begin')
 });
 
+// Configure static files
 app.use(express.static('public'));
 
+// Serve app on port
+const port = 6060;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
