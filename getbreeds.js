d3e1cd6933a40bd19674bdc30d871afc60bbd0bf
catbyte-io <catbyte.io@gmail.com>
2025-01-11 17:18:37 -0700

    Add dotenv for environment variables

--- getbreeds.js
+++ getbreeds.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const config = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const apiKey = config.apiKey;
+const apiKey = process.env.CAT_API_KEY;
 
 // Define URL parts
 axios.defaults.baseURL = "https://api.thecatapi.com/v1/"
@@ -12,8 +13,8 @@ axios.defaults.headers.common['x-api-key'] = apiKey;
 // Warrior cats character data
 const characters = [
   { name: 'Fireheart', link: '/fireheart', breed: 'Abyssinian'},
-  { name: 'Bluestar', link: '/bluestar'},
-  { name: 'Graystripe', link: '/graystripe'}
+  { name: 'Bluestar', link: '/bluestar', breed: 'Russian_Blue'},
+  { name: 'Graystripe', link: '/graystripe', breed: 'Maine_Coon'}
 ];
 
 async function getBreed(breed) {
@@ -22,6 +23,7 @@ async function getBreed(breed) {
             '/breeds',
             { params: {
                 breed: breed,
+                limit: 10,
             }}
         )
         return response.data;
@@ -30,10 +32,22 @@ async function getBreed(breed) {
     }
 };
 
-const searchImagesByBreed = async (breed)
+async function searchImagesByBreed(breed) {
     try {
-        const response = await axios.get();
+        const response = await axios.get(
+            'images/',
+            { params: {
+                breed: breed,
+                limit: 10,
+            }}
+        );
         return response.data;
     } catch (error) {
         console.error('There was an error fetching cat images');
-    };
+    }
+};
+
+module.exports = {
+    characters,
+    getBreed,
+};
